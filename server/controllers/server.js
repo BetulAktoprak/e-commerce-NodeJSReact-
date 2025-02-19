d4bcd8b1afcf43dabb269e07c7b5d3c560294acd
BetulAktoprak <145235075+BetulAktoprak@users.noreply.github.com>
2025-02-20 02:27:01 +0300

    user sınıfı için login, register, get, delete, put endpoindleri yazıldı

--- server/controllers/server.js
+++ server/controllers/server.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const categoryRoute = require("./categories");
 const productRoute = require("./products");
+const userRoute = require("./users");
+const authRoute = require("./auth");
 
 router.use("/categories", categoryRoute);
 router.use("/products", productRoute);
+router.use("/users", userRoute);
+router.use("/auth", authRoute);
 
 module.exports = router;
